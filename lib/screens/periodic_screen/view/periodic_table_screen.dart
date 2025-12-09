ef25c9716439a04691570ab48fef742c5b50f4a4
girishh <rootminusone001@gmail.com>
2025-12-09 10:53:20 +0530

    disposing controller

--- lib/screens/periodic_screen/view/periodic_table_screen.dart
+++ lib/screens/periodic_screen/view/periodic_table_screen.dart
@@ -24,6 +24,12 @@ class _PeriodicTableScreenState extends State<PeriodicTableScreen> with SingleTi
       );
     });
   }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
